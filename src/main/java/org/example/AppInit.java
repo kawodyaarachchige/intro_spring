a9f4af42289a607bc72a75345cfe5fda01888021
Tharushi Kawodya Arachchige <139765472+kawodyaarachchige@users.noreply.github.com>
2024-08-17 15:52:36 +0530

    added @Scope with PROTOTYPE

--- src/main/java/org/example/AppInit.java
+++ src/main/java/org/example/AppInit.java
@@ -2,6 +2,9 @@ package org.example;
 
 
 import org.example.config.Config;
+import org.example.obj.MyObj;
+import org.example.obj.OtherObj;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class AppInit {
@@ -10,7 +13,17 @@ public class AppInit {
         var ctx = new AnnotationConfigApplicationContext();
         ctx.register(Config.class);
         ctx.refresh();
-        ctx.close();
+        //
+        var otherObj = ctx.getBean(OtherObj.class);
+        System.out.println(otherObj.getClass().getName());
+        //
+        var myObj = ctx.getBean(MyObj.class);
+        System.out.println(myObj);
+        //
+        ConfigurableListableBeanFactory beanFactory = ctx.getBeanFactory();
+        System.out.println("is bean singleton: " + beanFactory.isSingleton("myObj"));
+
+        ctx.registerShutdownHook();
 
 
     }
--- src/main/java/org/example/obj/MyObj.java
+++ src/main/java/org/example/obj/MyObj.java
@@ -1,8 +1,11 @@
 package org.example.obj;
 
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
-@Component
+@Component("myObj")
+@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 public class MyObj {
     public MyObj() {
         System.out.println("Hello from MyObj");
