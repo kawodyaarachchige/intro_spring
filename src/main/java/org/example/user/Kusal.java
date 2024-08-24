4e4026d3a6aa051f72c668afbe489fa234ceb746
Tharushi Kawodya Arachchige <139765472+kawodyaarachchige@users.noreply.github.com>
2024-08-24 13:31:20 +0530

    DI with field injection

--- src/main/java/org/example/user/Kusal.java
+++ src/main/java/org/example/user/Kusal.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class Kusal {
-    //dependency injection
+    //dependency injection - field Injection
     @Autowired
      GoodGirl goodGirl;
 
