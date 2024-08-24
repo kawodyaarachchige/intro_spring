cac345d800862af8effa3b5e7a22f0972bbc9484
Tharushi Kawodya Arachchige <139765472+kawodyaarachchige@users.noreply.github.com>
2024-08-24 12:00:53 +0530

    implemented Bean Life cycle

--- src/main/java/org/example/config/Config.java
+++ src/main/java/org/example/config/Config.java
@@ -1,5 +1,6 @@
 package org.example.config;
 
+import org.example.obj.LifeCycle;
 import org.example.obj.MyObj;
 import org.example.obj.OtherObj;
 import org.springframework.context.annotation.Bean;
@@ -9,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 //@ComponentScan(basePackages = "org.example") main package or base package
 @ComponentScan(basePackageClasses = {MyObj.class})
+@ComponentScan(basePackageClasses = {LifeCycle.class})
 public class Config {
     @Bean
     OtherObj otherObj() {
---
+++ src/main/java/org/example/obj/LifeCycle.java
@@ -0,0 +1,46 @@
+package org.example.obj;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.*;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Component;
+
+import static org.springframework.aop.interceptor.ExposeBeanNameAdvisors.getBeanName;
+
+@Component("LifeCycle")
+public class LifeCycle implements BeanNameAware, BeanFactoryAware, ApplicationContextAware, InitializingBean, DisposableBean {
+
+    @Override
+    public void setBeanName(String name) {
+        System.out.println("Bean name is : " +name);
+
+    }
+
+    @Override
+    public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
+        System.out.println("Bean factory is : " +beanFactory);
+
+
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("Bean is initialized");
+
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        System.out.println("Application context is : " +applicationContext);
+
+    }
+    @Override
+    public void destroy() throws Exception {
+        System.out.println("Bean is destroyed");
+
+    }
+
+
+}
